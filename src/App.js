f78f4729ec2175ef0ec1db7236e0fb0fcc1802d2
hi10-b <bhudiahiten@gmail.com>
2021-04-19 22:04:07 +1000

    banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/home";
+
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return <div className="App"><Home /></div>;
 }
 
 export default App;
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from './banner'
+
+function Home() {
+    return (
+        <div>
+            <Banner Heading="Hiten Bhudia" />
+        </div>
+    )
+}
+
+export default Home
